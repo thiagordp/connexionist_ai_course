e32abd6f265819d0aba7892882942b01f4272d7f
Thiago R Dal Pont <thiagordalpont@gmail.com>
2022-07-15 21:40:58 -0300

    Added shallow NN experiment

--- final_project/src/modeling/cnn_classificier.py
+++ final_project/src/modeling/cnn_classificier.py
@@ -1,26 +1,58 @@
-from gensim.models import KeyedVectors
-from keras import Input, Model, metrics, regularizers
-from keras.callbacks import EarlyStopping
-from keras.layers import Embedding, Reshape, Conv2D, MaxPooling2D, concatenate, Flatten, Dropout, Dense
-from keras.optimizers import Adam, SGD
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
-import tensorflow as tf
+import logging
+import os
+import random
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import tqdm
+from gensim.models import KeyedVectors
+
 
 class CNN_Classifier():
 
-    def __init__(self, X_train, y_train, X_test, y_tes
-        t, embeddings_model):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
+    def __init__(self, embeddings_model, max_nb_words, embeddings_length,
+                 input_length):
         self.embeddings_model = embeddings_model
+        self.max_nb_words = max_nb_words
+        self.embeddings_length = embeddings_length
+        self.input_length = input_length
+        self.EMBEDDINGS_PATH = "Data/pre-trained_embeddings/"
 
-    def train(self):
-        pass
+    def get_vocab(self, vocab, text):
+        splits = text.split()
+
+        for token in splits:
+            if token not in vocab.keys():
+                vocab[token] = 1
+            else:
+                vocab[token] += 1
+
+    def load_embeddings(self):
+        ignore_list = [
+            "pre-trained_legal/glove_3500000000_100"
+        ]
+        matches = []
+        logging.info("Listing Embeddings")
+        for root, dirnames, filenames in os.walk(self.EMBEDDINGS_PATH):
+            for filename in filenames:
+                if filename.endswith('.txt'):
+                    path_emb = os.path.join(root, filename)
+
+                    matches.append(os.path.join(root, filename))
+
+        logging.info("Loading embeddings")
+        dict_list_embeddings = {}
+        random.shuffle(matches)
+
+        for path_emb in tqdm.tqdm(matches):
+            if path_emb.find("glove") != -1:
+
+                temp = get_tmpfile("glove2word2vec.txt")
+                glove2word2vec(path_emb, temp)
+
+                word_vectors = KeyedVectors.load_word2vec_format(temp, binary=False)
+            else:
+                word_vectors = KeyedVectors.load_word2vec_format(path_emb, binary=False)
+
+            key = path_emb.replace(self.EMBEDDINGS_PATH, "").replace(".txt", "")
+            dict_list_embeddings[key] = word_vectors
+
+        return dict_list_embeddings
--- final_project/src/modeling/util.py
+++ final_project/src/modeling/util.py
@@ -7,10 +7,10 @@ def get_class_weight(y):
     if y is None:
         raise RuntimeError("Impossible to calculate class weight because y is empty")
 
-    print("Calculating class weights")
+    #print("Calculating class weights")
     class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y), y=y)
-    print("Labels:", np.unique(y))
-    print("Class weights:", class_weights)
+    #print("Labels:", np.unique(y))
+    #print("Class weights:", class_weights)
     d_class_weights = dict(enumerate(class_weights))
 
     return d_class_weights
